a9698cfb33fa917d81a401a18afcfa9c71604f58
Aram Joseph <ajoseph@utc4me.org>
2017-01-17 10:25:21 -0500

    bla

---
+++ Assets/Scripts/DayCycle.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayCycle : MonoBehaviour
+{
+    private float dayTimer;
+    [SerializeField]
+    Color moonColor;
+    [SerializeField]
+    Color sunColor;
+    [SerializeField]
+    int dayLength;
+    [SerializeField]
+    int sunPower = 2;
+
+    private Light sun;
+
+    void Start()
+    {
+        dayTimer = dayLength / 2;
+        sun = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(dayTimer);
+        DayNight();
+    }
+
+
+    void DayNight()
+    {
+        dayTimer -= Time.deltaTime;
+        if (dayTimer <= 12)
+        {
+
+            sun.color = Color.Lerp(sun.color, moonColor, Time.deltaTime);
+
+
+            if (dayTimer < 0)
+            {
+                sun.color = sunColor;
+                dayTimer = dayLength;
+            }
+        }
+        sun.intensity = Mathf.Pow(((2 * dayTimer / dayLength) - 1), sunPower) * -1 + .8f;
+    }
+}
--- Assets/Scripts/GM.cs
+++ Assets/Scripts/GM.cs
@@ -5,21 +5,13 @@ using UnityEngine.UI;
 
 public class GM : MonoBehaviour
 {
-    [SerializeField]
-    int dayLength;
-    [SerializeField]
-    int sunPower = 2;
+
     [SerializeField]
     Text itemDisplay;
 
     public static GM instance = null;
-    private float dayTimer;
-    [SerializeField]
-     Color moonColor;
-    [SerializeField]
-     Color sunColor;
     public GameObject currHighlight;
-    private Light sun;
+
 
     // Use this for initialization
     void Awake()
@@ -30,18 +22,9 @@ public class GM : MonoBehaviour
             Destroy(gameObject);
     }
 
-    void Start()
-    {
-        dayTimer = dayLength/2;
-        sun = GameObject.Find("Sunlight").GetComponent<Light>();
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        //Debug.Log(dayTimer);
-        DayNight();
-    }
+
+
 
     public void ItemDisplay(GameObject thing /*, int itemType */)
     {
@@ -65,23 +48,5 @@ public class GM : MonoBehaviour
 
 
 
-    }
-
-    void DayNight()
-    {
-        dayTimer -= Time.deltaTime;
-        if (dayTimer <= 12)
-        {
-
-                sun.color = Color.Lerp(sun.color, moonColor, Time.deltaTime);
-
-            
-            if (dayTimer < 0)
-            {
-                sun.color = sunColor;
-                dayTimer = dayLength;
-            }
-        }
-        sun.intensity = Mathf.Pow(((2 * dayTimer / dayLength) - 1), sunPower) * -1 + .8f;
     }
 }
